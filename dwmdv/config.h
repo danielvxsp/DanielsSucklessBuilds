da3a1e3d28c16ba7a22eb32afe1fcae0ed842614
ninog <ninog@localhost.localdomain>
2025-01-30 20:54:39 +0100

    git issues

--- dwmdv/config.h
+++ dwmdv/config.h
@@ -1,9 +1,11 @@
 static unsigned int borderpx  	= 3;  /* border width */
 static unsigned int snap      	= 2;  /* snap pixel */
 static const unsigned int gappx = 9;  /* window gap pixel */ 
+static const int vertpad        = 0;  /* vertical padding of bar */
+static const int sidepad        = 0;  /* horizontal padding of bar */
 static int showbar            	= 1;   
 static int topbar             	= 1; 
-static const char *fonts[] 	= { "monospace:size=11" };
+static const char *fonts[]      = { "monospace:size=11" };
 static char dmenufont[]       	= "monospace:size=11";
 
 /* 
@@ -33,15 +35,14 @@ static const char *tags[] = { "1", "2", "3", "4" };
 static const Rule rules[] = {
 	/* class      	instance    	title       tags mask     isfloating     monitor */
 	{ "Gimp",     	NULL,       	NULL,       0,            1,             -1 },
-	{ "Firefox",  	NULL,       	NULL,       1 << 8,       0,             -1 },
-    	{ "mpv",      	NULL,       	NULL,       0,            1,             -1 },
+	{ "Firefox",  	NULL,       	NULL,       1 << 8,       0,             -1 },                                                           { "mpv",        NULL,           NULL,       0,            1,             -1 },
 };
 
 /* layout(s) */
-static float mfact     		= 0.55; /* factor of master area size [0.05..0.95] */
+static float mfact     		= 0.55; /* Master window size */
 static int nmaster     		= 1;    /* number of clients in master area */
 static int resizehints 		= 1;    /* 1 means respect size hints in tiled resizals */
-static int lockfullscreen 	= 0; /* 1 will force focus on the fullscreen window */
+static int lockfullscreen 	= 0;    /* 1 will force focus on the fullscreen window */
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    
@@ -65,8 +66,6 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "kitty", NULL };
-static const char *brupcmd[] = { "brightnessctl", "set", "10%+", NULL };
-static const char *brdowncmd[] = { "brightnessctl", "set", "10%-", NULL };
 
 /* Xresources preferences to load at startup */
 ResourcePref resources[] = {
@@ -92,41 +91,46 @@ ResourcePref resources[] = {
 
 #include <X11/XF86keysym.h>
 static const Key keys[] = {
-	/* modifier                     key        function            argument */
-	{ 0,           XF86XK_AudioLowerVolume,    spawn,              SHCMD("pamixer --decrease 5") },
-	{ 0,           XF86XK_AudioRaiseVolume,    spawn,              SHCMD("pamixer --increase 5") },
-	{ 0,           XF86XK_AudioMute,           spawn,              SHCMD("pamixer --toggle-mute") },
-	{ 0,           XF86XK_MonBrightnessUp,     spawn,              {.v = brupcmd} },
- 	{ 0,           XF86XK_MonBrightnessDown,   spawn,              {.v = brdowncmd} },
-	{ MODKEY,		        XK_r,      xrdb                {.v = NULL } },
-	{ MODKEY,                       XK_d,      spawn,              {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,              {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,          {0} },
-	{ MODKEY,                       XK_j,      focusstack,         {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,         {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,         {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_d,      incnmaster,         {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,           {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,           {.f = +0.05} },
-	{ MODKEY|ShiftMask,             XK_Return, zoom,               {0} },
-	{ MODKEY,                       XK_Tab,    view,               {0} },
-	{ MODKEY,                       XK_q,      killclient,         {0} },
-	{ MODKEY,                       XK_t,      setlayout,          {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,          {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,          {.v = &layouts[2]} },
-    { MODKEY,                       XK_space,  setlayout,          {0} },
-	{ MODKEY|ShiftMask,             XK_space,  togglefloating,     {0} },
-	{ MODKEY,                       XK_0,      view,               {.ui = ~0 } },
-	{ MODKEY|ShiftMask,             XK_0,      tag,                {.ui = ~0 } },
-	{ MODKEY,                       XK_comma,  focusmon,           {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,           {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,             {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,             {.i = +1 } },
-	TAGKEYS(                        XK_1,                          0)
-	TAGKEYS(                        XK_2,                          1)
-	TAGKEYS(                        XK_3,                          2)
-	TAGKEYS(                        XK_4,                          3)
-	{ MODKEY|ShiftMask,             XK_q,      quit,               {0} },
+	/* modifier                     key          function         argument */
+	{ 0,XF86XK_AudioLowerVolume,                 spawn,           SHCMD("pamixer --decrease 5 && pkill -RTMIN+1 slstatus") },
+	{ 0,XF86XK_AudioRaiseVolume,                 spawn,           SHCMD("pamixer --increase 5 && pkill -RTMIN+1 slstatus") },
+	{ 0,XF86XK_AudioMute,                        spawn,           SHCMD("pamixer --toggle-mute && pkill -RTMIN+1 slstatus") },
+	{ MODKEY,                       XK_Down,     spawn,           SHCMD("pamixer --decrease 5 && pkill -RTMIN+1 slstatus") },
+	{ MODKEY,                       XK_Up,       spawn,           SHCMD("pamixer --increase 5 && pkill -RTMIN+1 slstatus") },
+	{ MODKEY,                       XK_Left,     spawn,           SHCMD("pamixer --toggle-mute && pkill -RTMIN+1 slstatus") },
+    { 0,XF86XK_MonBrightnessUp,                  spawn,           SHCMD("brightnessctl set 10%+ && pkill -RTMIN+2 slstatus") },
+ 	{ 0,XF86XK_MonBrightnessDown,                spawn,           SHCMD("brightnessctl set 10%- && pkill -RTMIN+2 slstatus") },
+	{ MODKEY,                       XK_r,        xrdb,            {.v = NULL } },
+    { MODKEY,                       XK_minus,    setgaps,         {.i = +3 }, },
+    { MODKEY,                       XK_plus,     setgaps,         {.i = -3 }, },
+	{ MODKEY,                       XK_d,        spawn,           {.v = dmenucmd } },
+	{ MODKEY,                       XK_Return,   spawn,           {.v = termcmd } },
+	{ MODKEY,                       XK_b,        togglebar,       {0} },
+	{ MODKEY,                       XK_j,        focusstack,      {.i = +1 } },
+	{ MODKEY,                       XK_k,        focusstack,      {.i = -1 } },
+	{ MODKEY,                       XK_i,        incnmaster,      {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_d,        incnmaster,      {.i = -1 } },
+	{ MODKEY,                       XK_h,        setmfact,        {.f = -0.05} },
+	{ MODKEY,                       XK_l,        setmfact,        {.f = +0.05} },
+	{ MODKEY|ShiftMask,             XK_Return,   zoom,            {0} },
+	{ MODKEY,                       XK_Tab,      view,            {0} },
+	{ MODKEY,                       XK_q,        killclient,      {0} },
+	{ MODKEY,                       XK_t,        setlayout,       {.v = &layouts[0]} },
+	{ MODKEY,                       XK_f,        setlayout,       {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,        setlayout,       {.v = &layouts[2]} },
+    { MODKEY,                       XK_space,    setlayout,       {0} },
+	{ MODKEY|ShiftMask,             XK_space,    togglefloating,  {0} },
+	{ MODKEY,                       XK_0,        view,            {.ui = ~0 } },
+	{ MODKEY|ShiftMask,             XK_0,        tag,             {.ui = ~0 } },
+	{ MODKEY,                       XK_comma,    focusmon,        {.i = -1 } },
+	{ MODKEY,                       XK_period,   focusmon,        {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,    tagmon,          {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period,   tagmon,          {.i = +1 } },
+        { MODKEY|ShiftMask,             XK_q,        quit,            {0} },
+	TAGKEYS(                        XK_1,                         0)
+	TAGKEYS(                        XK_2,                         1)
+	TAGKEYS(                        XK_3,                         2)
+	TAGKEYS(                        XK_4,                         3)
 };
 
 /* button definitions */
